370aac3ceb0589d2e48cb9341e3c7ad8c36f05cf
Adam Land <adam.land1@network.lilly.com>
2025-10-16 16:22:27 -0400

    deploy auth

--- packages/container/src/components/AuthApp.js
+++ packages/container/src/components/AuthApp.js
@@ -2,7 +2,7 @@ import React, { useRef, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { mount } from 'auth/AuthApp';
 
-export default () => {
+export default ({ onSignIn }) => {
   const ref = useRef(null);
   const history = useHistory();
 
@@ -12,7 +12,8 @@ export default () => {
       onNavigate: ({ pathname: nextPathname }) => {
       if (history.location.pathname !== nextPathname) {
         history.push(nextPathname);
-      }}
+      }},
+      onSignIn
     });
     
     history.listen(onParentNavigate);
